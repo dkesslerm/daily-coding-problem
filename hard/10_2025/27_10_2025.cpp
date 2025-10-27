48898c13fca28cb71c6f432c9697669dd7a18e28
David Kessler <dkesslerm03@correo.ugr.es>
2025-10-27 20:10:37 +0100

    27_10_2025 UNDONE, too hard wow

---
+++ hard/10_2025/27_10_2025.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <cassert>
+using namespace std;
+
+// This is too hard wow
+// Daily Coding Problem: Problem #32 [Hard] - 27/10/25
+//
+// This problem was asked by Jane Street.
+// Suppose you are given a table of currency exchange
+// rates, represented as a 2D array. Determine whether
+// there is a possible arbitrage: that is, whether there
+// is some sequence of trades you can make, starting with
+// some amount A of any currency, such that you can end
+// up with an amount greater than A of that currency.
+//
+// There are no transaction costs, and you may trade
+// fractional quantities.
+
+class Solution {
+public:
+    bool detectArbitrage(vector<vector<double>> currencies) {
+
+        return true;
+    }
+};
+
+int main() {
+    // Daily Coding Problem: Problem #32 [Hard] - 27/10/25
+    Solution s;
+
+
+    return 0;
+}
