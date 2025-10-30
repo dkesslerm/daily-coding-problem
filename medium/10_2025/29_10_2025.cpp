66fd0a3c4e624242c89afc86211d36b8ae6ccba9
David Kessler <dkesslerm03@correo.ugr.es>
2025-10-30 12:36:21 +0100

    29_10_2025 with some coments, make it a bit clearer

--- medium/10_2025/29_10_2025.cpp
+++ medium/10_2025/29_10_2025.cpp
@@ -5,7 +5,6 @@ using namespace std;
 // Daily Coding Problem: Problem #34 [Medium] - 29/10/25
 //
 // This problem was asked by Quora.
-//
 // Given a string, find the palindrome that can be made by inserting the
 // fewest number of characters as possible anywhere in the word. If there
 // is more than one palindrome of minimum length that can be made, return
@@ -21,25 +20,12 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        int first = 0;
-        int last = s.size() - 1;
-
-        while (first < last) {
-            if (s[first] != s[last]) {
-                return false;
-            }
-            first++;
-            last--;
-        }
-
-        return true;
-    }
-
     string makePalindrome(string s) {
         int n = s.size();
+        // dp[i][j] indicates the smallest palindrome starting in i that ends in j
         vector<vector<string>> dp(n, vector<string>(n));
 
+        // A string starting and ending in the same spot is palindrome
         for (int i = 0; i < n; i++) {
             dp[i][i] = s[i];
         }
@@ -47,22 +33,24 @@ public:
         for (int len = 2; len <= n; len++) {
             for (int i = 0; i + len - 1 < n; i++) {
                 int j = i + len - 1;
-                if (s[i] == s[j]) {
+                if (s[i] == s[j]) { // if the chars are the same, it stays a palindrome
                     dp[i][j] = s[i] + dp[i + 1][j - 1] + s[j];
                 } else {
-                    string a = s[i] + dp[i + 1][j] + s[i];
-                    string b = s[j] + dp[i][j - 1] + s[j];
-                    if (a.size() < b.size()) {
+                    string a = s[i] + dp[i + 1][j] + s[i]; // rar
+                    string b = s[j] + dp[i][j - 1] + s[j]; // ara
+
+                    if (a.size() < b.size()) { // we want the smallest one
                         dp[i][j] = a;
-                    } else if (b.size() < a.size() ) {
+                    } else if (b.size() < a.size()) {
                         dp[i][j] = b;
-                    } else {
+                    } else { // first one alphabetically
                         dp[i][j] = min(a, b);
                     }
                 }
             }
         }
 
+        // We return the minimum palindrome of size n
         return dp[0][n - 1];
     }
 };
@@ -74,6 +62,5 @@ int main() {
     cout << s.makePalindrome("race") << endl;
 
     cout << s.makePalindrome("google") << endl;
-
     return 0;
 }
